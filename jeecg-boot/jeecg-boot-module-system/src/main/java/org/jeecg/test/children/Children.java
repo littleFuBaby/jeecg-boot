0c36216094f4ebe52c047782aa5331308036eb4a
andy <520fys008@163.com>
2022-04-16 23:18:34 +0800

    test string intern method

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/test/children/Children.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/test/children/Children.java
@@ -9,7 +9,41 @@ public class Children extends Parent {
     }
 
     public static void main(String[] args) {
+
+        String a = new String("x");
+
+        String b = "x";
+
+        System.out.println(a == b);
+
+        a.intern();
+
+        System.out.println(a == b);
+
+        String c = new String("x").intern();
+
+        String d = "x";
+
+        System.out.println(c == a);
+
+        System.out.println(c == d);
+
+
         // log.debug("语句");
+
+        float f1 = 10.2f;
+        float f2 = 10.2f;
+        float f3 = f1 * f2;
+
+        System.out.println(f3);
+
+        double d1 = 10.2d;
+        double d2 = 10.3d;
+        double d3 = d1 * d2;
+
+        System.out.println(d3);
+
+
     }
 
 }
