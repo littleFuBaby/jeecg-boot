067d05b3fadce2bc2e01e1dc5176b3500332b6e8
andy <520fys008@163.com>
2022-04-17 00:02:35 +0800

    test intern method of string class again

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/JeecgSystemApplication.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/JeecgSystemApplication.java
@@ -30,12 +30,12 @@ public class JeecgSystemApplication extends SpringBootServletInitializer {
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = oConvertUtils.getString(env.getProperty("server.servlet.context-path"));
-        log.info("\n----------------------------------------------------------\n\t" +
+        /*log.info("\n----------------------------------------------------------\n\t" +
                 "Application Jeecg-Boot is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
                 "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
                 "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
-                "----------------------------------------------------------");
+                "----------------------------------------------------------");*/
 
     }
 
--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/test/children/Children.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/test/children/Children.java
@@ -10,24 +10,18 @@ public class Children extends Parent {
 
     public static void main(String[] args) {
 
-        String a = new String("x");
+        String str1 = "Hello World!!!";
+        String str2 = new String("Hello World!!!");
+        String str3 = new String("Hello World!!!");
+        str3.intern();
+        String str4 = new String("Hello World!!!").intern();
 
-        String b = "x";
-
-        System.out.println(a == b);
-
-        a.intern();
-
-        System.out.println(a == b);
-
-        String c = new String("x").intern();
-
-        String d = "x";
-
-        System.out.println(c == a);
-
-        System.out.println(c == d);
-
+        System.out.println(str1 == str2); // false
+        System.out.println(str1 == str3); // false
+        System.out.println(str1 == str4); // true
+        System.out.println(str2 == str3); // false
+        System.out.println(str2 == str4); // false
+        System.out.println(str3 == str4); // false
 
         // log.debug("语句");
 
