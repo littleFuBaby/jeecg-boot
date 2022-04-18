2580257e9d8377d2a6bb795fd016ea009a9d549b
andy <520fys008@163.com>
2022-04-18 17:25:12 +0800

    super class

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/test/ExtendsInstance.java
@@ -0,0 +1,90 @@
+package org.jeecg.test;
+
+import java.util.Date;
+
+/**
+ * Notice difference between extending class relationship
+ * and simple java class implementing relationship different tables
+ * @author ys
+ */
+class SuperClass{
+    static{
+        honor = "NORMAL";
+        FAMILYCREATETIME = new Date();
+    }
+    public final static Date FAMILYCREATETIME;
+    private String name;
+    private static String honor;
+    public SuperClass(){
+    }
+    public SuperClass(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static String getHonor() {
+        return honor;
+    }
+    public static void setHonor(String honor) {
+        SuperClass.honor = honor;
+    }
+    public String getInformation(){
+        return "Honor --> " + honor + " | Name --> " + this.name + " | Super";
+    }
+    void print(){
+        System.out.println(this.getInformation());
+    }
+}
+class SubClass extends SuperClass{
+    public SubClass(){
+    }
+    public SubClass(String name){
+        // illegal syntax --> super *****************************
+        super();
+        this.setName(name);
+    }
+    public String getInformation(){
+        return "Honor --> " + getHonor() + " | Name --> " + this.getName() + " | Sub";
+    }
+    protected void print(){
+        // System.out.println(this.getInformation());
+        super.print();
+        // single use of super is wrong code convention
+        // System.out.println(super);
+        // super with . means super instance object
+    }
+}
+class SubSubClass extends SubClass{
+    public SubSubClass(String name){
+        this.setName(name);
+    }
+    public String getInformation(){
+        return "Honor --> " + getHonor() + " | Name --> " + this.getName() + " | Subsub";
+    }
+	/*
+	 * Incompatible print comparing with same method from superClass.
+	 * Because of return value type.
+	public boolean print(){
+		System.out.println(this.getInformation());
+		return true;
+	}
+	 */
+}
+public class ExtendsInstance {
+    public static void main(String[] args) {
+        SubSubClass subSub = new SubSubClass("jane");
+        subSub.print();
+        SubClass sub = new SubClass("andy");
+        SubClass.setHonor("UP");
+        sub.print();
+        SuperClass sup = new SuperClass("huangdi");
+        sup.print();
+        System.out.println(SubSubClass.FAMILYCREATETIME);
+        System.out.println(SubClass.FAMILYCREATETIME);
+        System.out.println(SuperClass.FAMILYCREATETIME);
+    }
+}
