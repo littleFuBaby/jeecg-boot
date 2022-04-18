caa2561233be7058519be87e46b50a4790db648a
andy <520fys008@163.com>
2022-04-18 15:21:54 +0800

    code block samples

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/test/CodeBlock.java
@@ -0,0 +1,14 @@
+package org.jeecg.test;
+
+public class CodeBlock {
+    // static code block
+    static { /* code */ }
+    // constructor code block
+    { /* code */ }
+    public void function(){
+        // normal code block
+        { /* code */ }
+        // synchronized code block
+        synchronized (this){ /* code */ }
+    }
+}
